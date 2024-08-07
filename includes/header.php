103068fbcbe5c71709aab1a222cb69db184711ee
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-08-07 23:01:39 +0200

    creating index.php

--- includes/header.php
+++ includes/header.php
@@ -37,9 +37,7 @@
 
         <!-- Page Content -->
         <div id="page-content-wrapper">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-                <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
-            </nav>
+
             <div class="container-fluid">
 </body>
 
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include 'includes/header.php'; ?>
+
+<h1>Addi Test 1</h1>
+<p>this is a test sentence</p>
+
+<?php include 'includes/footer.php' ?>
