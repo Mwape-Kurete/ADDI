3798f628d8726e7398044fef49a66163edfe6111
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-08-08 16:19:30 +0200

    login+signup cycle
    not yet complete, but the bones are setup

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,41 @@
+<?php
+session_start(); //starts / resumes a session
+require '../includes/db.php'; //ensures that the db.php file (containing our connection is placed)
+
+//checking if the request method is POST, which indicates the form has been submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    //fetching values of submitted form
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Define the SQL query to insert the username and password into the users table
+    $sql = 'INSERT INTO users (add your query here)';
+
+    // prepare the SQL statement for execution 
+    // -> symbol is the object operator (Js would be: user.age | PHP is user->age)
+    $stmt = $conn->prepare($sql);
+
+
+    // Add the values of the user's input into the SQL statement
+    $stmt->bind_param("ss", $username, $password); // "ss" means two string parameters
+
+    // If the prepared statement is executed
+    if ($stmt->execute()) {
+        echo "Registration Complete"; // If execution is successful, display a success message
+    } else {
+        echo "Error" . $sql . "<br>" . $conn->error; // If there is an error, display the error message along with the SQL query and connection error
+    }
+
+    // Close the statement
+    $stmt->close();
+
+    // Close the database connection
+    $conn->close();
+}
+?>
+
+
 <?php include '../includes/header.php'; ?>
 
 <div class="row login-form">
@@ -6,13 +44,13 @@
             <!-- form inputs -->
             <h6>Email/Username:</h6>
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" name="formId1" id="formId1" placeholder="" />
-                <label for="formId1">Email/Username</label>
+                <input type="text" class="form-control" name="formId1" id="username" placeholder="" />
+                <label for="username">Email/Username</label>
             </div>
             <h6>Password:</h6>
             <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password">
+                <label for="password">Password</label>
             </div>
 
             <!-- submit button  -->
--- pages/register.php
+++ pages/register.php
@@ -1,18 +1,64 @@
+<?php
+session_start(); //starts / resumes a session
+require '../includes/db.php'; //ensures that the db.php file (containing our connection is placed)
+
+//checking if the request method is POST, which indicates the form has been submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    //fetching values of submitted form
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = 'SELECT * FROM users WHERE (add your query here)';
+
+    // prepare the SQL statement for execution 
+    // -> symbol is the object operator (Js would be: user.age | PHP is user->age)
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('ss', $username, $password); //'s' means string parameter 
+
+    //execute the SQL statement 
+    $stmt->execute();
+
+    //storing the result 
+    $result = $stmt->get_result();
+
+    //checks if the result is not empty 
+    if ($result->num_rows > 0) {
+        // fetching the data retrieved from the associative array and storing it in user variable... i think 
+        $user = $result->fetch_assoc();
+
+        if ($password === $user['password']) {
+            //start a session and store 
+            $_SESSION['username'] = $user['id'];
+            header('Location: index.php');
+            exit();
+        } else {
+            echo 'Invalid username/password';
+        }
+    } else {
+        echo 'Invalid username/password';
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
+
 <?php include '../includes/header.php'; ?>
 
 <div class="container-fluid register-form">
-    <form action="">
+    <form action="POST" action="register.php">
 
         <!-- base info -->
         <h6>Full Name:</h6>
         <div class="form-floating mb-3">
-            <input type="text" class="form-control" name="formId1" id="formId1" placeholder="" />
-            <label for="formId1">Full Name</label>
+            <input type="text" class="form-control" name="formId1" id="fullname" placeholder="" />
+            <label for="fullname">Full Name</label>
         </div>
         <h6>Email Address:</h6>
         <div class="form-floating mb-3">
-            <input type="email" class="form-control" name="formId1" id="formId1" placeholder="" />
-            <label for="formId1">Email Address</label>
+            <input type="email" class="form-control" name="formId1" id="email" placeholder="" />
+            <label for="email">Email Address</label>
         </div>
 
         <!-- password -->
@@ -20,8 +66,8 @@
             <h6>Password:</h6>
             <div class="col">
                 <div class="form-floating mb-3">
-                    <input type="password" class="form-control" name="formId1" id="formId1" placeholder="" />
-                    <label for="formId1">Password</label>
+                    <input type="password" class="form-control" name="password" id="formId1" placeholder="" />
+                    <label for="password">Password</label>
                 </div>
             </div>
             <div class="col">
@@ -35,8 +81,8 @@
         <!-- bio and more info -->
         <h6>Bio:</h6>
         <div class="form-floating">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 170px"></textarea>
-            <label for="floatingTextarea2">Bio</label>
+            <textarea class="form-control" placeholder="Leave a comment here" id="bio" style="height: 170px"></textarea>
+            <label for="bio">Bio</label>
         </div>
 
         <br>
