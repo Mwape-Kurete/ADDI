4f2e860728acbff77be3cf7865eff5440075f580
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-08-10 20:02:58 +0200

    adding specific css files

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,8 @@
     <!-- Bootstrap Links -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="/ADDI/assets/css/main.css" rel="stylesheet">
+    <link href="/ADDI/assets/css/templates.css" rel="stylesheet">
+    <link href="/ADDI/assets/css/includes.css" rel="stylesheet">
 
 </head>
 
