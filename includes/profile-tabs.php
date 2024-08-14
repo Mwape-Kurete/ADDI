00a691c5b94fe45cf4d9b2c7ff2755dbfdc58190
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-08-14 14:35:44 +0200

    adding home tabs and profile tabs css

--- includes/profile-tabs.php
+++ includes/profile-tabs.php
@@ -1,12 +1,12 @@
 <!--Profile Tabs Start-->
-<ul class="nav nav-tabs profile-tabs">
-    <li class="nav-item">
+<ul class="nav nav-tabs">
+    <li class="nav-item profile-nav-tabs">
         <a class="nav-link active" aria-current="page" href="#">Answered</a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item profile-nav-tabs">
         <a class="nav-link" href="#">Asked</a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item profile-nav-tabs">
         <a class="nav-link" href="#">Saved</a>
     </li>
 </ul>
--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 <div class="row">
     <div class="col main-pageContent home-tabs">
         <?php include 'includes/home-tabs.php' ?>
+        <div class="home-content-events">
+
+        </div>
+
     </div>
     <div class="col activityPanel">
         <?php include 'includes/activity_panel.php'; ?>
