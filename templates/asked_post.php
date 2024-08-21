18c561af6ad5e00c91f0dcff04b0cccc4c202719
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-08-22 00:38:33 +0200

    styling template elements

--- templates/asked_post.php
+++ templates/asked_post.php
@@ -1,9 +1,10 @@
 <!--START OF ASKED POST/CARD-->
-<div class="col-12 ask-card">
-    <div class="card" style="width: 100%;">
+<div class="col-10 ask-card">
+    <div class="card ask-card-cont" style="width: 100%;">
         <div class="card-body">
             <h5 class="card-title">Ask title</h5>
             <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, nisi facilis? Voluptatem recusandae eum consequatur placeat inventore ut accusamus, hic reiciendis suscipit cum minus. Nobis doloribus incidunt culpa ipsum laborum.</p>
+            <p class="event-ask-tag">#<span class="event-ask-type">event-tag</span></p>
             <div class="row lower-half">
                 <div class="col-11 meta-info">
                     <a href="#" class="card-link posted-by">@<span>username</span></a>
@@ -16,8 +17,8 @@
         </div>
     </div>
 </div>
-<div class="col-12 show-thread">
-    <button type="button" class="btn btn-primary view-answers-asks"> View Answer Thread <i class="bi bi-chevron-compact-down"></i></button>
+<div class="col-10 show-thread mx-auto">
+    <button type="button" class="btn btn-primary view-answers-asks-btn"> View Answer Thread <i class="bi bi-chevron-compact-down"></i></button>
 </div>
 
 <!--END OF ASKED POST/CARD-->
--- templates/event_post.php
+++ templates/event_post.php
@@ -1,12 +1,14 @@
 <!--START OF EVENT CARD-->
 
 <div class="col-12 event-card">
-    <div class="card" style="width: 100%;">
+    <div class="card event-card-cont" style="width: 85%;">
         <div class="card-body">
             <h5 class="card-title event-title">Event title</h5>
             <p class="card-text event-details">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
             <p>When: <strong class="date-of-event">1 august 2024</strong></p>
             <p>Where: <strong class="location-of-event">A Place</strong></p>
+            <p class="event-ask-tag">#<span class="event-ask-type">event-tag</span></p>
             <div class="row lower-half">
                 <div class="col-11 meta-info">
                     <a href="#" class="card-link posted-by">@<span>username</span></a>
@@ -20,7 +22,7 @@
         <a
             name="view-event-comments"
             id="view-event-comments"
-            class="btn btn-primary"
+            class="btn btn-primary view-event-comments-btn"
             href="#"
             role="button">View Comments</a>
 
